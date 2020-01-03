0ad7d8eada72150c09616cc075ec306f25a4750d
jevvilla <juan.es-19@hotmail.com>
2020-01-03 16:43:52 -0500

    Details screen updated to implement Carousel component

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {NavigationStackProp} from 'react-navigation-stack';
 
-const Details: React.FC = () => {
+import {Carousel} from './components';
+import {Hotel} from '../../common/intefaces';
+import {styles} from './styles';
+
+interface Props {
+  navigation: NavigationStackProp<Hotel>;
+}
+
+const Details: React.FC<Props> = ({navigation}) => {
+  const {images} = navigation.state.params?.hotel;
+
   return (
-    <View>
-      <Text>Details</Text>
+    <View style={styles.container}>
+      <Carousel images={images} />
     </View>
   );
 };
