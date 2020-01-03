92c6dfa1980e339170956f710ce50ad098b714a0
jevvilla <juan.es-19@hotmail.com>
2020-01-03 06:05:51 -0500

    HotelList component created, this component is in charge of rendering the list of hotels displayed in the Home screen

---
+++ src/screens/Home/components/HotelList/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {FlatList} from 'react-native';
+import {SafeAreaView} from 'react-navigation';
+
+import {Hotel} from '../../../../common/intefaces';
+import HotelCard from '../HotelCard';
+import {styles} from './styles';
+
+interface Props {
+  onCardPress: () => void;
+  data: Hotel[];
+}
+
+const HotelList: React.FC<Props> = ({onCardPress, data}) => {
+  const keyExtractor = (item: Hotel) => item.id;
+
+  const renderItem = ({item}: {item: Hotel}) => {
+    return <HotelCard onPress={onCardPress} style={styles.card} hotel={item} />;
+  };
+
+  return (
+    <FlatList
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={keyExtractor}
+      showsVerticalScrollIndicator={false}
+      keyboardShouldPersistTaps="handled"
+      contentContainerStyle={styles.flatlist}
+      ListFooterComponent={<SafeAreaView forceInset={{bottom: 'always'}} />}
+    />
+  );
+};
+
+export default HotelList;
---
+++ src/screens/Home/components/HotelList/styles.ts
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+import {sizes} from '../../../../common/styles';
+
+export const styles = StyleSheet.create({
+  flatlist: {paddingHorizontal: sizes.base * 2},
+  card: {
+    width: '100%',
+    marginVertical: sizes.base * 2,
+  },
+});
