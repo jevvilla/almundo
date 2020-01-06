07cb5c784ca69f9ef5e8481180a80f9d0705d39d
jevvilla <juan.es-19@hotmail.com>
2020-01-06 08:35:15 -0500

    SplashScreen hide after app starts up, StatusBar color changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import Router from './navigation';
+import {colors} from 'src/common/styles';
 
 const App: React.FC = () => {
-  return <Router />;
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <>
+      <StatusBar backgroundColor={colors.background} barStyle="dark-content" />
+      <Router />
+    </>
+  );
 };
 
 export default App;
